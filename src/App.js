ab65f7b02646966c2d048fd98ba5e69ef36da7f7
Elijah Bernstein-Cooper <ebernstein-cooper@lexile.com>
2022-04-11 06:29:20 -0400

    added session work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import MindsetQuestions from './MindsetQuestions';
+import Answer from './Answer';
+
+/* Lesson 1-3 learning objectives
+
+Understand how to handle changes the user is making to input fields on your site
+Understand how object data can be passed between components
+Understand how state allows data to be altered/page content to be updated over time
+Understand how callback handlers are used to share data up the component tree
+
+References
+- javascript events: https://developer.mozilla.org/en-US/docs/Web/Events
+- prevent default usage: 
+
+
+*/
+
+
+/* Topics covered in lesson
+
+1. handle changes from input 
+2. pass data between components: props
+3. handle changes over time
+4. use callback handlers to share data up the tree
+
+*/
 
 function App() {
+  const [answers, setAnswers] = useState('')
   return (
     <div>
-    <h1>Hello World</h1>
+      <Answer onSubmit={
+        function(event)  {
+          setAnswers(answers + ', ' + event.target.title.value);
+          event.preventDefault();
+        }
+      }
+      
+        onRender={()=> console.log('answer rendered')}
+      />
+      <p>{answers}</p>
     </div>
   );
 }
+
 
 export default App;
